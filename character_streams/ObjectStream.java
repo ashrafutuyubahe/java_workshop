1425a4632f6a434540c9aaf672768429636096b0
Ashrafu <147812120+ashrafutuyubahe@users.noreply.github.com>
2025-01-14 11:25:25 +0200

    first commit

---
+++ character_streams/ObjectStream.java
@@ -0,0 +1,11 @@
+// import java.io.*;
+// inport Student 
+
+
+// public class ObjectStream {
+    
+
+//   public  static   void main(String [] args){
+//       Student s= new Student("john","smith")();
+//   }  
+// }
